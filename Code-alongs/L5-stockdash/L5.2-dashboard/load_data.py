2b552b486d9c85f2afc372bc0bab00a0c8202089
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-11-02 16:39:03 +0100

    test med interative python, class and function

--- Code-alongs/L5-stockdash/L5.2-dashboard/load_data.py
+++ Code-alongs/L5-stockdash/L5.2-dashboard/load_data.py
@@ -16,9 +16,14 @@ class StockDataLocal:
 
         for path_ending in ["_TIME_SERIES_DAILY.csv", "_TIME_SERIES_INTRADAY_EXTENDED.csv"]:
             path = self._data_folder_path + stockname + path_ending
+            print(path)
             stock = pd.read_csv(path, index_col = 0, parse_dates = True)
             stock.index.rename("Date", inplace=True)
 
             stock_df_list.append(stock)
 
         return stock_df_list
+
+## test med interative python
+# sdl = StockDataLocal()
+# sdl.stock_dataframe("IBM")
