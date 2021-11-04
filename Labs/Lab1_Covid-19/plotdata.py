538baa070fb1125170d82473b6ad72f8ffe311e4
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-11-05 00:58:19 +0100

    Add plotdata.py to plot. Write short conclusions

---
+++ Labs/Lab1_Covid-19/plotdata.py
@@ -0,0 +1,18 @@
+import plotly_express as px
+
+def time_series_plot(df, y_variables):
+    for y_variable in y_variables:
+        fig = px.line(df, x="Vecka", y=y_variable, color='Region',
+                    symbol="Region", title=y_variables[y_variable])
+        fig.show()
+        fig.write_html(
+            f"Visualiseringar/Q3.{y_variables[y_variable]}.html", auto_open=False)
+
+def scatter_plot(df, y_variables, x):
+    for y_variable in y_variables:
+        fig = px.scatter(df, x=x, y=y_variable,
+                 color="Region", size="Befolkning", size_max=70,
+                 log_x=False, animation_frame="Vecka", title=f"{y_variables[y_variable]} vs {x}",
+                 range_y=[0,df[y_variable].quantile(0.95)])
+        fig.show()
+        fig.write_html(f'Visualiseringar/Q3.{y_variables[y_variable]} vs {x}.html', auto_open=True)
